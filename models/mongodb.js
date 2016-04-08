ee819dd84263bfc87015300987a6cff6af6f0afa
administator <administator>
2016-04-08 09:10:47 +0800

    整理项目结构

--- models/mongodb.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var db = mongoose.createConnection('localhost', 'mongo-test');
-
-db.on('error', console.error.bind(console, '连接错误：'));
-db.once('open', function () {
-	//
-	console.log('ssss');
-});
-exports.mongoose = mongoose;
